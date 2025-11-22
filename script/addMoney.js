c1c201cbb4393c383fc6ad2a2eda2ee330770f08
toha245 <mdabdulawal2001@gmail.com>
2025-11-22 22:42:17 +0600

    sixth commit

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,17 +1,41 @@
-document.getElementById('add-money').addEventListener('click', function(event){
-    event.preventDefault();
-    const amount = document.getElementById("amount").value;
-    const convertedAmount = parseFloat(amount);
-    const pin = document.getElementById('pin').value;
-    const convertedPin = parseInt(pin);
-    const mainBalance = document.getElementById('main-balance').innerText;
-    const convertedMainBalance = parseFloat(mainBalance);
+// document.getElementById('add-money').addEventListener('click', function(event){
+//     event.preventDefault();
+//     const amount = document.getElementById("amount").value;
+//     const convertedAmount = parseFloat(amount);
+//     const pin = document.getElementById('pin').value;
+//     const convertedPin = parseInt(pin);
+//     const mainBalance = document.getElementById('main-balance').innerText;
+//     const convertedMainBalance = parseFloat(mainBalance);
    
-    if(convertedPin === 1234){
-        const sum = convertedMainBalance + convertedAmount;
-        document.getElementById('main-balance').innerText = sum;
-    }
-    else{
-        console.log('pin sothik na');
-    }
+//     if(convertedPin === 1234){
+//         const sum = convertedMainBalance + convertedAmount;
+//         document.getElementById('main-balance').innerText = sum;
+//     }
+//     else{
+//         console.log('pin sothik na');
+//     }
+// })
+
+// reapetetion komabo ei code diye
+document.getElementById("add-money").addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        const amount = getInputValueById("amount");
+        const pin = getInputValueById("pin");
+        const account = document.getElementById("account-number").value;
+        const mainBalance = getInnerTextById("main-balance");
+        if(account.length === 11){
+            if(pin === 1234){
+                const sum = mainBalance + amount;
+                // document.getElementById("main-balance").innerText = sum;
+                setInnerTextByIDAndValue("main-balance", sum);
+                console.log(setInnerTextByIDAndValue);
+            }
+            else{
+                console.log("pin thik nai")
+            }
+        }
+        else{
+            console.log("account number thik nai")
+        }
 })
---
+++ utilities/utilities.js
@@ -0,0 +1,15 @@
+function getInputValueById (id){
+    const value = document.getElementById(id).value;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function getInnerTextById(id){
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function setInnerTextByIDAndValue (id, value){
+    document.getElementById(id).innerText = value;
+}
